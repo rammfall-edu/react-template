9b2a31211ded99e59309f7d33551a292c720427b
rammfall <15bogdan17@gmail.com>
2022-06-17 01:55:03 +0300

    Complete template

--- src/Application/component.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Application = () => {
-  return <div>test</div>;
-};
-
-export default Application;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const { join, resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (config) => {
-  console.log(config);
+module.exports = (_, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
   return {
     entry: './src/index.js',
     output: {
@@ -17,21 +20,53 @@ module.exports = (config) => {
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
-              plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset/resource', // use /inline if dont work
+          parser: {
+            dataUrlCondition: {
+              limit: 30 * 1024,
             },
+          },
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[hash][ext][query]',
           },
         },
       ],
     },
     devServer: {
+      hot: true,
       port: 3000,
       static: {
         directory: join(__dirname, 'src/static/'),
       },
       historyApiFallback: true,
     },
-    devtool: 'inline-source-map',
+    devtool: isDevelopment
+      ? 'eval-cheap-module-source-map'
+      : 'hidden-nosources-source-map',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/static/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[fullhash].css',
+      }),
+    ],
   };
 };
